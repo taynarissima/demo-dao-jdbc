221526b93cd2088dd8d365a887a2e87e4a7bffc8
Taynara Sousa <so.taynara@gmail.com>
2024-01-09 16:06:57 -0300

    insert implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -21,5 +21,10 @@ public class Program2 {
 		for (Department d : list) {
 			System.out.println(d);
 		}
+		
+		System.out.println("\n==== TEST 3: department insert ====");
+		Department newDepartment = new Department(null, "Music");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
 	}
 }
--- src/model/dao/impl/DepartmentDaoImplJDBC.java
+++ src/model/dao/impl/DepartmentDaoImplJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,8 +23,36 @@ public class DepartmentDaoImplJDBC implements DepartmentDao {
 
 	@Override
 	public void insert(Department obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO department "
+					+ "(Name) "
+					+ "VALUES "
+					+ "(?)",
+					Statement.RETURN_GENERATED_KEYS);
 
+			st.setString(1, obj.getName());
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -43,10 +72,8 @@ public class DepartmentDaoImplJDBC implements DepartmentDao {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement(
-					"SELECT * FROM department "
-					+ "WHERE department.Id = ?");
-			
+			st = conn.prepareStatement("SELECT * FROM department " + "WHERE department.Id = ?");
+
 			st.setInt(1, id);
 			rs = st.executeQuery();
 
